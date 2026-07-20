9f7147f6a6dd1549077f1d4585e6846ab9d59f00
Carlos Téllez Sandoval <st4502@utr.edu.mx>
2026-07-20 12:59:00 -0600

    token final final

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,23 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    createBook,
-    getBooks,
-    getBookById,
-    updateBook,
-    deleteBook
+  createBook,
+  getBooks,
+  getBookById,
+  updateBook,
+  deleteBook,
 } = require("../controllers/bookController");
 
 router.post("/", createBook);
-router.get(
-    "/",
-    (req, res, next) => {
-        return res.status(401).json({
-            message: "Middleware desde bookRoutes"
-        });
-    },
-    getBooks
-);
+router.get("/", getBooks);
 router.get("/:id", getBookById);
 router.put("/:id", updateBook);
 router.delete("/:id", deleteBook);
